f1aed0e967c16322a7bbf001639e235a5076fe7c
Илья Филиппов <filippov.i.s@gmail.com>
2021-12-03 18:29:42 +0300

    Добавьте файлы проекта.

---
+++ Program_6.2_/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_6._2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string urok = Console.ReadLine();
+            urok = urok.Replace(" ", "");
+            urok = urok.ToLower();
+            char[] mas = urok.ToCharArray();
+
+            Array.Reverse(mas);
+            string koru = "";
+            koru = new string(mas);
+
+            if (urok == koru)
+            {
+                Console.WriteLine("Строка является полиандромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является полиандромом!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
